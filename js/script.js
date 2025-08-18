829dc3b29a853113a0d1ed96d014de21bfeda891
zaklab <zakariya.khelifa2012@gmail.com>
2025-08-18 23:33:21 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>ZakCorp</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <!-- Ton contenu ici -->
+
+  <!-- Inclusion du JS juste avant la fermeture du body -->
+  <script src="js/script.js"></script>
+</body>
+</html>
